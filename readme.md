cc2ca8ad575c7740be277d1618976beead620881
nikhil.karkra@rmit.edu.au <nikhil.karkra@rmit.edu.au>
2019-05-29 11:18:25 +1000

    es6 setup

---
+++ readme.md
@@ -0,0 +1,7 @@
+#ES6 started kit
+
+1) Build with webpack 4
+2) Using babel 7 
+
+To start the kit run
+npm run dev
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import style from "./main.css";
+console.log('hey')
+const arr = [1, 2, 3];
+const es6Demo = () => console.log(...arr);
+es6Demo()
